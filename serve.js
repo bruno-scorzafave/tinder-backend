8c41c24c677d178b4446eeaa308ccda8041961ff
BrunoScorzafave <bruno.scorzafave@scandiweb.com>
2023-05-18 17:51:07 -0300

    First commit - Create backend

---
+++ serve.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Cors from 'cors';
+
+import Cards from './dbCards.js'
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://admin:ujqMnV2fIy4hvGkE@cluster0.qvgpg4e.mongodb.net/local_library?retryWrites=true&w=majority';
+
+// Middlewares
+app.use(express.json());
+app.use(Cors());
+
+// DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send("Hello coder"));
+
+app.post('/tinder/cards', async (req, res) => {
+    const dbCard = req.body;
+
+    try {
+        const responseCards = await Cards.create(dbCard);
+        res.status(200).send(responseCards);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.get('/tinder/cards', async (req, res) => {
+    try {
+        const cardsResult = await Cards.find();
+        res.status(200).send(cardsResult);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Listener
+app.listen(port, () => console.log(`listening on port ${port}`));
